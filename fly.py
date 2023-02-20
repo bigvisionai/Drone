36793512132e9f9117a194bd1055d4e8a3149af5
kXborg <kukilp213@gmail.com>
2023-02-20 16:18:39 +0530

    integrated coco object detection pipeline

--- fly.py
+++ fly.py
@@ -1,16 +1,9 @@
-import keyboard as key 
+import cv2
 from time import sleep 
+import keyboard as key 
 from djitellopy import tello 
-import cv2 
-
-# Initializations.
-key.init()
-drone = tello.Tello()
-drone.connect()
-print(drone.get_battery())
-drone.streamon()
-
-out = cv2.VideoWriter('rec.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 25, (960, 720))
+from detection import *
+from ultralytics import YOLO 
 
 def controls():
     lr, fb, ud, rot = 0, 0, 0, 0
@@ -34,14 +27,27 @@ def controls():
     return [lr, fb, ud, rot]
 
 
+# Initializations.
+key.init()
+drone = tello.Tello()
+drone.connect()
+print(drone.get_battery())
+drone.streamon()
+
+# Create the video writer.
+out = cv2.VideoWriter('rec.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 25, (960, 720))
+model = YOLO('yolov8n.pt')
+
+
 while True:
     vals = controls()
     drone.send_rc_control(vals[0], vals[1], vals[2], vals[3])
 
     img = drone.get_frame_read().frame
+    det_img = draw_predictions(img.copy(), model)
     print(img.shape)
-    cv2.imshow('Stream', img)
-    out.write(img)
+    cv2.imshow('Stream', det_img)
+    out.write(det_img)
     wait = cv2.waitKey(1)
     if wait == ord('q'):
         break
@@ -50,4 +56,6 @@ while True:
         print('Program Terminated')
         print('\n Thanks for flying Tello.')
         break
+
+# Release the video writer.
 out.release()
